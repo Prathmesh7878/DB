12685d1552e3a42929aa1e2658bc002636465a48
Prathmesh7878 <154068934+Prathmesh7878@users.noreply.github.com>
2026-03-15 00:37:50 +0530

    added database schema and seed data

--- DBAnalyzer/src/main/java/org/example/Main.java
+++ DBAnalyzer/src/main/java/org/example/Main.java
@@ -1,12 +1,13 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.config.DBConnection;
+
+import java.sql.Connection;
+
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws Exception {
+
+        Connection conn = DBConnection.getConnection();
 
 
     }
--- DBAnalyzer/src/main/java/org/example/config/DBConnection.java
+++ DBAnalyzer/src/main/java/org/example/config/DBConnection.java
@@ -1,32 +1,39 @@
 package org.example.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class DBConnection {
 
-    private static final Dotenv dotenv =
-            Dotenv.configure()
-                    .directory("./")
-                    .ignoreIfMissing()
-                    .load();
+    private static final String URL = "jdbc:mysql://localhost:3306/olist";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to MySQL database!");
+
+            Statement stmt = conn.createStatement();
+            String q = "select customer_id from customers";
+            ResultSet rs = stmt.executeQuery(q);
+
+            int i=0;
+            while (rs.next() && i<=5){
+                String data = rs.getString("customer_id");
+                System.out.println(data);
+                i++;
+            }
 
 
-    System.out.println("HOST=" + HOST);
-    System.out.println("PORT=" + PORT);
-    System.out.println("DB=" + DB);
 
-//    private static final String HOST = dotenv.get("DB_HOST");
-//    private static final String PORT = dotenv.get("DB_PORT");
-//    private static final String DB = dotenv.get("DB_NAME");
-//    private static final String USER = dotenv.get("DB_USER");
-//    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
-//
-//    private static final String URL =
-//            "JDBC:mysql://" + HOST + ":" + PORT + "/" + DB;
-//
-//    public static Connection getConnection() throws Exception {
-//        return DriverManager.getConnection(URL, USER, PASSWORD);
-//    }
+        } catch (SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
 }
